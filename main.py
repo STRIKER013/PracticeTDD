8e85d1b093e49e3d9c48d8285691f3d1f69e1af0
STRIKER013 <66685038+STRIKER013@users.noreply.github.com>
2023-05-09 17:02:00 -0500

    #Fase 2 isPrime
    Implementación de la funcion isPrime y realizando una prueba obvia

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from typing import Union
-
 from fastapi import FastAPI
-
-#from FuncionesPY import hellowFastApiPY
 
 app = FastAPI()
 
 
 @app.get("/")
 def helloFastApiPy():
-    return {"Hello FastApi"}
+    return "Hello FastApi"
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
